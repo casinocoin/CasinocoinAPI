49e0a39ce2e2c4528400ca5f6312e5e6203c56e0
Andre Jochems <andre@jochems.com>
2016-11-14 14:18:58 +0100

    cleanup

---
+++ Casinocoin-Public-API/sql/005_create_exchange_history.sql
@@ -0,0 +1,14 @@
+USE DATABASE csc_public_api;
+
+CREATE TABLE csc_public_api.exchange_history
+(
+  id int(255) NOT NULL AUTO_INCREMENT,
+  short_name varchar(20) NOT NULL,
+  bid_price_btc DECIMAL(20,8) NULL,
+  ask_price_btc DECIMAL(20,8) NULL,
+  price_btc DECIMAL(20,8) NULL,
+  volume24h DECIMAL(20,8) NULL,
+  creation_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT ECS_PK PRIMARY KEY (id)
+)
+COMMENT = 'Casinocoin Exchange History table';
